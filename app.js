da10a076338e18b9311698acc737be0debc2684a
Treast <vincent.riva@outlook.fr>
2019-01-09 17:49:16 +0100

    Keep all workers and send them new data

--- app.js
+++ app.js
@@ -20,42 +20,34 @@ const ffmpeg = require('fluent-ffmpeg');
 ffmpeg.setFfmpegPath(ffmpegPath);
 
 const CPUS = os.cpus();
-const workers = [];
 const results = [];
 let images = [];
 
-const workerEvent = new EventEmitter();
+const getNextImage = () => {
+    return images.shift()
+}
 
 const createWorker = () => {
   return new Promise(resolve => {
-    const image = images.shift();
+    const image = getNextImage()
 
     if (!image) {
       resolve();
     }
 
     const worker = childProcess.fork(path.resolve(__dirname, 'worker.js'), ['--image', image]);
+    
     worker.on('message', ({ clusters }) => {
       results.push(clusters);
-    });
+      const nextImage = getNextImage()
 
-    worker.on('exit', () => {
-      const workerIndex = workers.indexOf(worker);
-      worker.kill();
-
-      if (workerIndex > -1) workers.splice(workerIndex, 1);
-
-      if (images.length === 0) {
-        if (workers.length === 0) {
-          workerEvent.emit('handle:quantification');
-        }
-        resolve();
+      if(nextImage) {
+          worker.send({image: nextImage})
+      } else {
+          worker.kill()
+          resolve()
       }
-
-      return createWorker();
     });
-
-    workers.push(worker);
   });
 };
 
@@ -90,15 +82,11 @@ const processScreenshots = () => {
       });
 
       Promise.all(promises).then(() => {
-        console.log('Promise finished');
+          handleQuantification()
       });
     });
   });
 };
-
-workerEvent.once('handle:quantification', () => {
-  handleQuantification();
-});
 
 console.time('Timer');
 getScreenshotsFromVideo(args.video)
--- worker.js
+++ worker.js
@@ -3,42 +3,50 @@ const Quantification = require('./core/quantification/Quantification');
 const { createCanvas, loadImage } = require('canvas');
 const path = require('path')
 
-console.log(`Worker ${process.pid} with ${args.image}`)
+const processImage = (img) => {
+    console.log(`Worker ${process.pid} with ${img}`)
 
-loadImage(path.resolve(__dirname, 'images', args.image))
-    .then(image => {
-        const WIDTH = image.width;
-        const HEIGHT = image.height;
+    loadImage(path.resolve(__dirname, 'images', img))
+        .then(image => {
+            const WIDTH = image.width;
+            const HEIGHT = image.height;
 
-        const canvas = createCanvas(WIDTH, HEIGHT);
-        const ctx = canvas.getContext('2d');
-        ctx.clearRect(0, 0, WIDTH, HEIGHT);
+            const canvas = createCanvas(WIDTH, HEIGHT);
+            const ctx = canvas.getContext('2d');
+            ctx.clearRect(0, 0, WIDTH, HEIGHT);
 
-        ctx.drawImage(image, 0, 0);
-        const pixels = ctx.getImageData(0, 0, WIDTH, HEIGHT).data;
-        const data = [];
+            ctx.drawImage(image, 0, 0);
+            const pixels = ctx.getImageData(0, 0, WIDTH, HEIGHT).data;
+            const data = [];
 
-        for (let i = 0; i < pixels.length; i += 4 * 20) {
-            data.push([pixels[i], pixels[i + 1], pixels[i + 2]]);
-        }
+            for (let i = 0; i < pixels.length; i += 4 * 20) {
+                data.push([pixels[i], pixels[i + 1], pixels[i + 2]]);
+            }
 
-        let quantification = new Quantification(data, 8);
-        quantification.generate();
+            let quantification = new Quantification(data, 8);
+            quantification.generate();
 
-        quantification.clusters.sort((a, b) => {
-            return b.clusterData.length - a.clusterData.length;
-        });
+            quantification.clusters.sort((a, b) => {
+                return b.clusterData.length - a.clusterData.length;
+            });
 
-        const clustersData = []
+            const clustersData = []
 
-        for (let cluster of quantification.clusters) {
-            clustersData.push([cluster.vector.x, cluster.vector.y, cluster.vector.z]);
-        }
+            for (let cluster of quantification.clusters) {
+                clustersData.push([cluster.vector.x, cluster.vector.y, cluster.vector.z]);
+            }
 
-        process.send({
-            clusters: clustersData
+            process.send({
+                clusters: clustersData
+            })
         })
-    })
-    .catch(() => {
-        console.log('Worker error')
-    });
+        .catch(() => {
+            console.log('Worker error')
+        });
+}
+
+process.on('message', (message) => {
+    processImage(message.image)
+})
+
+processImage(args.image)
