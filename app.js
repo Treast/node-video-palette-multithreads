20ac0bd974eb5e11f1342b263e26f92820664b4c
Treast <vincent.riva@outlook.fr>
2019-01-09 18:36:36 +0100

    Add output image

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const defaults = {
   quality: 20,
-  width: 1280,
+  width: 1920,
+  height: 1080,
   output: false,
   video: './Trailer - Blade Runner 2049.mp4',
   fps: 1,
@@ -19,8 +20,11 @@ const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
 const ffmpeg = require('fluent-ffmpeg');
 ffmpeg.setFfmpegPath(ffmpegPath);
 
+const Quantification = require('./core/quantification/Quantification');
+const { createCanvas, loadImage } = require('canvas');
+
 const CPUS = os.cpus();
-const results = [];
+let results = [];
 let images = [];
 
 const getNextImage = () => {
@@ -38,7 +42,7 @@ const createWorker = () => {
     const worker = childProcess.fork(path.resolve(__dirname, 'worker.js'), ['--image', image]);
     
     worker.on('message', ({ clusters }) => {
-      results.push(clusters);
+      results = [...results, ...clusters]
       const nextImage = getNextImage()
 
       if(nextImage) {
@@ -53,7 +57,43 @@ const createWorker = () => {
 
 const handleQuantification = () => {
   console.timeEnd('Timer');
+
+  const quantification = new Quantification(results, 8)
+  quantification.generate()
+
+  quantification.clusters.sort((a, b) => {
+    return b.clusterData.length - a.clusterData.length;
+  })
+
+  quantification.clusters.map(cluster => console.log(cluster.getHex()))
+  
+  return generatePostImage(quantification.clusters)
 };
+
+const generatePostImage = clusters => {
+  return new Promise(resolve => {
+    const localCanvas = createCanvas(args.width, args.height);
+    const localCtx = localCanvas.getContext('2d');
+    const colorWidth = args.width / Math.floor(clusters.length / 2);
+    const colorHeight = args.height / 2
+
+    const pivot = Math.floor(clusters.length / 2);
+    for (let i = 0; i < clusters.length; i += 1) {
+      let height = 0;
+      if (i >= pivot) {
+        height += colorHeight;
+      }
+      localCtx.fillStyle = clusters[i].getHex();
+      localCtx.fillRect(colorWidth * (i % pivot), height, colorWidth, colorHeight);
+    }
+    const out = fs.createWriteStream(path.resolve(__dirname, 'dist', `${args.video}.jpg`));
+    const stream = localCanvas.createJPEGStream();
+    stream.pipe(out);
+    out.on('finish', () => {
+      resolve();
+    });
+  })
+}
 
 const getScreenshotsFromVideo = video => {
   return new Promise((resolve, reject) => {
@@ -88,9 +128,11 @@ const processScreenshots = () => {
   });
 };
 
+console.time('TimerFull');
 console.time('Timer');
 getScreenshotsFromVideo(args.video)
   .then(processScreenshots)
   .then(() => {
     console.log('Finished');
+    console.timeEnd('TimerFull')
   });
